d25c5ecf66ef362f2dc3da3346ba0baee982f127
Chaitra Sarathy <chaitrasarathy@gmail.com>
2021-01-19 11:12:59 +0100

    mod: cutoff selection using kneepoint

--- src/selectPCs.m
+++ src/selectPCs.m
@@ -1,23 +1,21 @@
-function [pcs_selected_cond1, pcs_selected_cond2] = selectPCs(selectedCols, cutoff, numIters, numComps_cond1)
+function [pcs_selected_cond1, pcs_selected_cond2, cutoff, features_n_freq] = selectPCs(selectedCols, numIters, numComps_cond1)
 
     features_n = cat(1, selectedCols.all{:});
     features_n_freq = sortrows(tabulate(features_n), 2, 'descend');
     features_n_freq(:,4) = features_n_freq(:,2)/numIters*100;
-%     plot(features_n_freq(:,4), 'black','LineWidth',2)
-%     xlim([-10 1040])
-%     ylim([-5 100])
-%     set(gca, 'FontSize', 18, 'FontWeight','bold')
-%     xlabel("Index of features")
-%     ylabel("Frequency of estimation (%)")
-%     axis square
+    [~, cutoff] = knee_pt(features_n_freq(:,4),1:length(features_n_freq));
 
-    % Criteria: top 20% of the features
-    pcs_selected = features_n_freq(1:round(length(features_n_freq)*cutoff), 1);
+
+   % OLD Criteria: top 20% of the features
+   %  pcs_selected = features_n_freq(1:round(length(features_n_freq)*cutoff), 1);
+
+    % Criteria: cut off is the knee point of the frequency curve
+    pcs_selected = features_n_freq(1:cutoff, 1);
     
     % convert serial index to condition specific index
     % pcs_selected = sort([colp;coln]);
     inds = pcs_selected > numComps_cond1;
-    pcs_selected_cond1 = pcs_selected(pcs_selected < numComps_cond1); %pcs_selected(1:(min(inds)-1));
+    pcs_selected_cond1 = pcs_selected(pcs_selected <= numComps_cond1); %pcs_selected(1:(min(inds)-1));
     pcs_selected_cond2 = pcs_selected(inds)-numComps_cond1;
 
 
